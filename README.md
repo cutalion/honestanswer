013086d2d0de132195a22ba66a57e19305d1d80c
Alexander Glushkov <cutalion@gmail.com>
2011-09-28 01:03:19 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+Honest Answer
+=============
+
+# Description
+
+Gives user an opportunity to ask any question and to get
+an absolutely fair (anonymous) answer.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Honestanswer::Application.config.secret_token = 'ff840b66c16b9532dd6a5e940c14109ca55d88a1063f9df22a14fb366939a7a3cc00177792602ac2815a078243df1c0ce78b8fecc4c896fd745ba302f74f16ab'
