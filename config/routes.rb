1c828620508bf7fbbbbe6c0f36785fe5e4b3efb9
Alexander Glushkov <cutalion@gmail.com>
2011-09-30 02:15:18 +0300

    Home page stub

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Honestanswer::Application.routes.draw do
+  get "welcome/index"
+
+  root :to => "welcome#index"
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
