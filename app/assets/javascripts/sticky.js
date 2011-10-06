cbaf6e96ef6c31d3f505a3d0f228a9004ba5be4a
Alexander Glushkov <cutalion@gmail.com>
2011-10-07 02:07:25 +0300

    Sticky form

---
+++ app/assets/javascripts/sticky.js
@@ -0,0 +1,21 @@
+(function($) {
+  $.fn.extend({
+    sticky: function() {
+      var header = $(this),
+          origTop = header.offset().top;
+
+      $(window).scroll(function(e) {
+        if ($(this).scrollTop() > origTop) {
+          header.addClass("sticky");
+        } else if ($(this).scrollTop() < origTop) {
+          header.removeClass("sticky");
+        }
+      });
+    }
+  });
+
+  $(function() {
+    $('form').sticky();
+  });
+}(jQuery));
+
